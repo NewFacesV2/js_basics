ff4d171fe8678fd004a36bf2abb8792b8a3aec12
NewFacesV2 <Iain-Henderson@live.nmit.ac.nz>
2022-02-23 19:13:07 +1300

    we added an index JS file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen (-)
+// Are case sensetive
+
+
+let firstName = 'Mosh';
+let lastName = 'Henderson';
+
